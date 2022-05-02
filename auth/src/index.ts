dd0935b2175a2a4216480a6bcb5dd31183d54c51
staticbass <ostrenkovlad.dev@gmail.com>
2022-05-02 18:56:04 +0100

    mongo url var

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -3,13 +3,14 @@ import { app } from './app';
 
 const start = async () => {
   const port = 3000
+  const mongoURL = 'mongodb://auth-mongo-srv:27017/auth'
 
   if (!process.env.JWT_KEY) {
     throw new Error('JWT_KEY must be defined');
   }
 
   try {
-    await mongoose.connect('mongodb://auth-mongo-srv:27017/auth');
+    await mongoose.connect(mongoURL);
     console.log('Connected to MongoDb');
   } catch (err) {
     console.error(err);
